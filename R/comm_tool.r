1d6d597c5e588c649f17283a1f4d885b4b43a2c4
snoweye <wccsnow@gmail.com>
2013-02-15 11:38:45 -0500

    comm.stop and comm.warning

---
+++ R/comm_tool.r
@@ -0,0 +1,43 @@
+### S3 tool function except print() and cat() see "spmd_tool.r".
+
+comm.any <- function(x, na.rm = FALSE, comm = .SPMD.CT$comm){
+  ret <- spmd.allgather.integer(any(x, na.rm = na.rm),
+                                integer(comm.size(comm)), comm = comm)
+  any(ret, na.rm = na.rm)
+} # End of comm.any().
+
+comm.all <- function(x, na.rm = FALSE, comm = .SPMD.CT$comm){
+  ret <- spmd.allgather.integer(all(x, na.rm = na.rm),
+                                integer(comm.size(comm)), comm = comm)
+  all(ret, na.rm = na.rm)
+} # End of comm.all().
+
+comm.stop <- function(..., call. = TRUE, domain = NULL,
+    all.rank = .SPMD.CT$print.all.rank, rank.print = .SPMD.CT$rank.source,
+    comm = .SPMD.CT$comm, quiet = TRUE, sep = " ", fill = FALSE,
+    labels = NULL, append = FALSE, flush = TRUE, con = stdout()){
+  spmd.comm.cat(..., all.rank = all.rank, rank.print = rank.print, comm = comm,
+                quiet = quiet, sep = sep, fill = fill, labels = labels,
+                append = append, flush = flush, con = con)
+
+  options(show.error.messages = FALSE)
+  stop(..., call. = call., domain = domain)
+
+  invisible()
+} # End of comm.stop().
+
+comm.warning <- function(..., call. = TRUE, immediate. = FALSE, domain = NULL,
+    all.rank = .SPMD.CT$print.all.rank, rank.print = .SPMD.CT$rank.source,
+    comm = .SPMD.CT$comm, quiet = TRUE, sep = " ", fill = FALSE,
+    labels = NULL, append = FALSE, flush = TRUE, con = stdout()){
+  spmd.comm.cat(..., all.rank = all.rank, rank.print = rank.print, comm = comm,
+                quiet = quiet, sep = sep, fill = fill, labels = labels,
+                append = append, flush = flush, con = con)
+
+  if(spmd.comm.rank(comm) == rank.print || all.rank == TRUE){
+    warning(..., call. = call., immediate. = immediate., domain = domain)
+  }
+
+  invisible()
+} # End of comm.warning().
+
