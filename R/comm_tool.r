4ff28bccc941e7047b486e8f7da7a5cbb7c228f1
snoweye <wccsnow@gmail.com>
2013-02-15 16:47:41 -0500

    stop, warning, warnings

--- R/comm_tool.r
+++ R/comm_tool.r
@@ -12,32 +12,29 @@ comm.all <- function(x, na.rm = FALSE, comm = .SPMD.CT$comm){
   all(ret, na.rm = na.rm)
 } # End of comm.all().
 
-comm.stop <- function(..., call. = TRUE, domain = NULL,
+comm.stop <- function(...,
     all.rank = .SPMD.CT$print.all.rank, rank.print = .SPMD.CT$rank.source,
     comm = .SPMD.CT$comm, quiet = TRUE, sep = " ", fill = FALSE,
     labels = NULL, append = FALSE, flush = TRUE, con = stdout()){
   spmd.comm.cat(..., all.rank = all.rank, rank.print = rank.print, comm = comm,
                 quiet = quiet, sep = sep, fill = fill, labels = labels,
                 append = append, flush = flush, con = con)
-
-  options(show.error.messages = FALSE)
-  stop(..., call. = call., domain = domain)
-
-  invisible()
+  finalize()
 } # End of comm.stop().
 
 comm.warning <- function(..., call. = TRUE, immediate. = FALSE, domain = NULL,
-    all.rank = .SPMD.CT$print.all.rank, rank.print = .SPMD.CT$rank.source,
-    comm = .SPMD.CT$comm, quiet = TRUE, sep = " ", fill = FALSE,
-    labels = NULL, append = FALSE, flush = TRUE, con = stdout()){
-  spmd.comm.cat(..., all.rank = all.rank, rank.print = rank.print, comm = comm,
-                quiet = quiet, sep = sep, fill = fill, labels = labels,
-                append = append, flush = flush, con = con)
-
+    all.rank = .SPMD.CT$print.all.rank, comm = .SPMD.CT$comm){
   if(spmd.comm.rank(comm) == rank.print || all.rank == TRUE){
     warning(..., call. = call., immediate. = immediate., domain = domain)
   }
-
   invisible()
 } # End of comm.warning().
+
+comm.warnings <- function(...,
+    all.rank = .SPMD.CT$print.all.rank, rank.print = .SPMD.CT$rank.source,
+    comm = .SPMD.CT$comm, quiet = TRUE){
+  if(spmd.comm.rank(comm) == rank.print || all.rank == TRUE){
+    warnings(...)
+  }
+} # End of comm.warnings().
 
