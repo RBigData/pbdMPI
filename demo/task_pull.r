2cd7daf04fbaebec9f579a4a303005b5587425af
snoweye <wccsnow@gmail.com>
2013-06-20 16:03:36 -0400

    update

--- demo/task_pull.r
+++ demo/task_pull.r
@@ -1,12 +1,16 @@
-library(pbdDEMO, quiet = TRUE)
+### SHELL> mpiexec -np 2 Rscript --vanilla [...].r
 
+### Initial
+library(pbdDMPI, quiet = TRUE)
+
+### Examples
 FUN <- function(jid){
   Sys.sleep(1)
   jid * 10
 }
 
 ret <- task.pull(1:10, FUN)
-
 comm.print(ret)
 
+### Finish
 finalize()
