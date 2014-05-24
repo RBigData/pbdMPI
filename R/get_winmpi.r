06d83139b6def05eaeabea7027a119c8c89a7c64
snoweye <wccsnow@gmail.com>
2014-05-23 22:48:38 -0400

    updates

--- R/get_winmpi.r
+++ R/get_winmpi.r
@@ -2,142 +2,14 @@
 ### possible MPI dynamic/static library from the environment variable
 ### "MPI_ROOT" preset by users.
 
-### Get library flags.
-get.winlib <- function(arch = c("32", "64"), type = c("MS-MPI", "MPICH2")){
-  mpi.root <- Sys.getenv(paste("MPI_ROOT_", arch[1], sep = ""))
-
-  if(type[1] == "MS-MPI"){
-    ### MS-MPI
-    if(arch[1] == "32"){
-      arch.c <- "i386"
-    } else{
-      arch.c <- "amd64"
-    }
-    flag.found <- FALSE
-
-    mpi.home <- Sys.getenv("MPI_HOME")
-    if(mpi.home != ""){
-      ### Try Winbuilder
-      lib.file <- try(paste(mpi.home, "Lib/", arch.c, "/msmpi.lib", sep = ""))
-      if(file.exists(lib.file)){
-        # mpi.lib <- shortPathName(lib.file)
-        mpi.lib <- gsub("\\\\", "/", lib.file)
-        mpi.lib <- paste("\"", mpi.lib, "\"", sep = "")
-        cat(mpi.lib)
-        flag.found <- TRUE
-      }
-    } else{
-      ### Try WCC's way
-      lib.file <- try(paste(mpi.root, "Lib/", arch.c, "/msmpi.lib", sep = ""))
-      if(file.exists(lib.file)){
-        # mpi.lib <- shortPathName(lib.file)
-        mpi.lib <- gsub("\\\\", "/", lib.file)
-        mpi.lib <- paste("\"", mpi.lib, "\"", sep = "")
-        cat(mpi.lib)
-        flag.found <- TRUE
-      }
-    }
-
-    if(!flag.found){
-      ### Try MS-MPI default
-      mpi.lib <- Sys.getenv(paste("MSMPI_LIB", arch[1], sep = ""))
-      lib.file <- try(paste(mpi.lib, "msmpi.lib", sep = ""))
-      if(file.exists(lib.file)){
-        # mpi.lib <- shortPathName(lib.file)
-        mpi.lib <- gsub("\\\\", "/", lib.file)
-        mpi.lib <- paste("\"", mpi.lib, "\"", sep = "")
-        cat(mpi.lib)
-      } else{
-        cat("msmpi.lib is not found.\n")
-      }
-    }
-  } else{
-    ### MPICH2
-    lib.file <- try(paste(mpi.root,
-                          c("bin/libmpi.dll", "lib/libmpi.a"), sep = ""))
-    for(i.file in lib.file){
-      if(file.exists(i.file)){
-        mpi.lib <- gsub("(.*)(/libmpi.*)", "\\1", i.file)
-        mpi.lib <- paste("-L\"", mpi.lib, "\" -lmpi", sep = "")
-        cat(mpi.lib)
-        break
-      }
-    }
-  }
-
-  invisible()
-} # End of get.winlib().
-
-
-### Get include flags.
-get.wininc <- function(arch = c("32", "64"), type = c("MS-MPI", "MPICH2")){
-  mpi.root <- Sys.getenv(paste("MPI_ROOT_", arch[1], sep = "") )
-
-  if(type[1] == "MS-MPI"){
-    ### MS-MPI
-    flag.found <- FALSE
-
-    mpi.home <- Sys.getenv("MPI_HOME")
-    if(mpi.home != ""){
-      ### Try Winbuilder
-      inc.file <- try(paste(mpi.home, "Inc/mpi.h", sep = ""))
-      if(file.exists(inc.file)){
-        flag.found <- TRUE
-        inc.dir <- paste(mpi.home, "Inc/", sep = "")
-        # mpi.inc <- shortPathName(inc.dir)
-        mpi.inc <- gsub("\\\\", "/", inc.dir)
-        cat(mpi.inc)
-      }
-    } else{
-      ### Try WCC's setup
-      inc.file <- try(paste(mpi.root, "Inc/mpi.h", sep = ""))
-      if(file.exists(inc.file)){
-        flag.found <- TRUE
-        inc.dir <- try(paste(mpi.root, "Inc/", sep = ""))
-        # mpi.inc <- shortPathName(inc.dir)
-        mpi.inc <- gsub("\\\\", "/", inc.dir)
-        cat(mpi.inc)
-      }
-    }
-
-    if(!flag.found){
-      ### Try MS-MPI default
-      mpi.inc <- Sys.getenv("MSMPI_INC")
-      inc.file <- try(paste(mpi.inc, "mpi.h", sep = ""))
-      if(file.exists(inc.file)){
-        inc.dir <- try(paste(mpi.inc, sep = ""))
-        # mpi.inc <- shortPathName(inc.dir)
-        mpi.inc <- gsub("\\\\", "/", inc.dir)
-        cat(mpi.inc)
-      } else{
-        cat("mpi.h is not found.\n")
-      }
-    }
-  } else{
-    ## MPICH2
-    inc.file <- try(paste(mpi.root, "include/mpi.h", sep = ""))
-    inc.file <- gsub("\\\\", "/", inc.file)
-
-    if(!file.exists(inc.file)){
-      cat("mpi.h is not found.\n")
-    }
-
-    mpi.inc <- gsub("(.*)(/mpi.h)", "\\1", inc.file)
-    cat(mpi.inc)
-  }
-
-  invisible()
-} # End of get.wininc().
-
-
 ### Get conversion.
 get.sysenv <- function(flag){
   sysenv <- Sys.getenv(flag)
   # sysenv <- shortPathName(sysenv)
   sysenv <- gsub("\\\\", "/", sysenv)
-  # if(length(grep("/$", sysenv)) == 0){
-  #   sysenv <- paste(sysenv, "/", sep = "")
-  # }
+  if(length(grep("/$", sysenv)) == 0){
+    sysenv <- paste(sysenv, "/", sep = "")
+  }
   cat(sysenv)
 
   invisible()
