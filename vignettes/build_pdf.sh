e28e43487e57c2a8cde0f4776d12da548dec050e
snoweye <wccsnow@gmail.com>
2014-06-21 15:59:30 -0400

    update

--- vignettes/build_pdf.sh
+++ vignettes/build_pdf.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-rm *.aux *.bbl *.blg *.log *.out *.toc
+R_HOME=`Rscript -e "cat(R.home()[1])"`
+JSS_BST=${R_HOME}/share/texmf/bibtex/bst/jss.bst
+JSS_CLS=${R_HOME}/share/texmf/tex/latex/jss.cls
+
+rm *.aux *.bbl *.blg *.log *.out *.toc *.bst *.cls
+cp ${JSS_BST} ./
+cp ${JSS_CLS} ./
+
+rm *.aux *.bbl *.blg *.log *.out *.toc *.bst *.cls
 pdflatex pbdMPI-guide.Rnw
 bibtex pbdMPI-guide
 pdflatex pbdMPI-guide.Rnw
 pdflatex pbdMPI-guide.Rnw
 pdflatex pbdMPI-guide.Rnw
-rm *.aux *.bbl *.blg *.log *.out *.toc
 
+rm *.aux *.bbl *.blg *.log *.out *.toc *.bst *.cls
 mv -f *.pdf ../inst/doc/
 cp -f *.Rnw ../inst/doc/
