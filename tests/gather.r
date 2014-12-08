8dded3a3c2463b7e5fb85cf912269521672104aa
snoweye <wccsnow@gmail.com>
2014-12-07 19:10:59 -0500

    fix dynload mergeing

--- tests/gather.r
+++
@@ -1,9 +0,0 @@
-suppressPackageStartupMessages(library(pbdMPI, quietly=TRUE))
-init()
-
-testval <- length(allgather(comm.rank()))
-trueval <- comm.size()
-
-if (comm.rank()==0) testval == trueval
-
-finalize()
