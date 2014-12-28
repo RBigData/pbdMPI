22dbb76fde654f7ad60f5533664598079a22f276
snoweye <wccsnow@gmail.com>
2014-12-27 23:57:01 -0500

    typo

--- R/000_globalVariables.r
+++ R/000_globalVariables.r
@@ -1,7 +1,7 @@
 ### Do not delete this file and file name!!
 ### This file should be loaded before all other *.r files.
 
-### This is to avoid the fale positive messages from R CMD check.
+### This is to avoid the false positive messages from R CMD check.
 ###   "no visible binding for global variable"
 ### Suggested by Prof Brian Ripley
 ### ?globalVariables
