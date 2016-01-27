5acf9ae39c53361f02ff6f95572460b713ed7777
wccfda <wccfda@iastate.edu>
2016-01-27 16:31:23 -0500

    Drop internal sealed .pbd_env

--- R/222_export_env.r
+++ R/222_export_env.r
@@ -1,12 +1,12 @@
 ### Export control to new env.
-.pbd_env <- new.env()
-.pbd_env$SPMD.CT <- SPMD.CT()
-.pbd_env$SPMD.OP <- SPMD.OP()
-.pbd_env$SPMD.IO <- SPMD.IO()
-.pbd_env$SPMD.TP <- SPMD.TP()
+# .pbd_env <- new.env()
+# .pbd_env$SPMD.CT <- SPMD.CT()
+# .pbd_env$SPMD.OP <- SPMD.OP()
+# .pbd_env$SPMD.IO <- SPMD.IO()
+# .pbd_env$SPMD.TP <- SPMD.TP()
 
 ### For non-blocking calls.
-.pbd_env$SPMD.NB.BUFFER <- list()
+# .pbd_env$SPMD.NB.BUFFER <- list()
 
 
 ### The above are fake. These only be here for reference and to fool
