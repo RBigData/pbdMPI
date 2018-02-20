6899e7afb1944251d7335f1d6f6aeb09c18e91d8
wccsnow <wccsnow@gmail.com>
2018-02-19 20:06:18 -0500

    More test

--- R/util_execmpi.r
+++ R/util_execmpi.r
@@ -1,31 +1,41 @@
 ### Utility
 
-execmpi <- function(spmd.code, mpicmd = "mpiexec", nranks = 2L,
-    temp = tempfile(),
-    intern = FALSE, ignore.stdout = FALSE, ignore.stderr = FALSE,
-    wait = TRUE){
-  ### Input checks
-  if(!is.character(spmd.code)){
-    stop("argument 'spmd.code' must be a character string")
-  } else if(length(spmd.code) == 0){
-    stop("argument 'spmd.code' must be a non-empty character string")
-  } else if (length(spmd.code) > 1){
-    warning("spmd.code has length > 1; only the first element will be used")
-    spmd.code <- spmd.code[1L]
-  }
+execmpi <- function(spmd.code = NULL, spmd.file = NULL, mpicmd = "mpiexec",
+    nranks = 2L, intern = FALSE, ignore.stdout = FALSE,
+    ignore.stderr = FALSE, wait = TRUE){
+  ### Check # of ranks.
   nranks <- as.integer(nranks)
   if(nranks <= 0){
     stop("argument 'nranks' must be a integer and greater than 0.")
   }
 
-  ### Dump spmd.code to temp file, execute
-  # cat(spmd.code, file = temp)
-  conn <- file(temp, open="wt")
-  writeLines(spmd.code, conn)
-  close(conn)
+  ### Input checks
+  if(! is.null(spmd.code)){
+    if(!is.character(spmd.code)){
+      stop("argument 'spmd.code' must be a character string")
+    } else if(length(spmd.code) == 0){
+      stop("argument 'spmd.code' must be a non-empty character string")
+    } else if (length(spmd.code) > 1){
+      warning("spmd.code has length > 1; only the first element will be used")
+      spmd.code <- spmd.code[1L]
+    }
+
+    ### Dump spmd.code to a temp file, execute
+    spmd.file <- tempfile()
+    conn <- file(spmd.file, open = "wt")
+    writeLines(spmd.code, conn)
+    close(conn)
+  } else{
+    if(is.null(spmd.file)){
+      stop("Either spmd.code or spmd.file should be provided.")
+    }
+  }
+  if(! file.exists(spmd.file)){
+    stop("spmd.file does not exist.")
+  }
 
   ### Make a cmd.
-  cmd <- paste(mpicmd, "-np", nranks, "Rscript", temp, sep = " ")
+  cmd <- paste(mpicmd, "-np", nranks, "Rscript", spmd.file, sep = " ")
   print(cmd)
   system(cmd, intern = intern, ignore.stdout = ignore.stdout,
          ignore.stderr = ignore.stderr, wait = wait)
