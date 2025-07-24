caab1531af52c741e7f65886c2371cff4a6d5df5
George Ostrouchov <gostrouc@utk.edu>
2025-07-24 11:32:05 -0400

    first debug trigger

--- inst/debug/testUnload.R
+++ inst/debug/testUnload.R
@@ -1,8 +1,12 @@
 # This is a test script for pbdMPI
+print("About to load pbdMPI")
 library(pbdMPI)
+print("Loaded pbdMPI")
 sessionInfo()
 print("Size and Rank")
 print(comm.size())
 print(comm.rank())
+print("About to finalize pbdMPI")
 finalize()
+print("Finalized pbdMPI")
 # test unloading of pbdMPI?
