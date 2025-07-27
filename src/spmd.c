526327cc6867b2318251bfc6c656803a5394c782
George Ostrouchov <gostrouc@utk.edu>
2025-07-27 16:28:47 -0400

    try calloc?

--- src/spmd.c
+++ src/spmd.c
@@ -36,7 +36,7 @@ SEXP spmd_initialize(void){
 	MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
 	MPI_Comm_set_errhandler(MPI_COMM_SELF, MPI_ERRORS_RETURN);
 	if(global_spmd_comm == NULL){
-		global_spmd_comm = (MPI_Comm *) R_Calloc(global_spmd_COMM_MAXSIZE, MPI_Comm); 
+		global_spmd_comm = (MPI_Comm *) Calloc(global_spmd_COMM_MAXSIZE, MPI_Comm); 
 		global_spmd_comm[0] = MPI_COMM_WORLD;
 		for(i = 1; i < global_spmd_COMM_MAXSIZE; i++){
 			global_spmd_comm[i] = MPI_COMM_NULL;
@@ -48,17 +48,17 @@ SEXP spmd_initialize(void){
 	}
 #endif
 	if(global_spmd_status == NULL){
-		global_spmd_status = (MPI_Status *) R_Calloc(global_spmd_STATUS_MAXSIZE, MPI_Status); 
+		global_spmd_status = (MPI_Status *) Calloc(global_spmd_STATUS_MAXSIZE, MPI_Status); 
 	}
 	if(global_spmd_datatype == NULL){
-		global_spmd_datatype = (MPI_Datatype *) R_Calloc(1, MPI_Datatype); 
+		global_spmd_datatype = (MPI_Datatype *) Calloc(1, MPI_Datatype); 
 	}
 	if(global_spmd_info == NULL){
-		global_spmd_info = (MPI_Info *) R_Calloc(1, MPI_Info);
+		global_spmd_info = (MPI_Info *) Calloc(1, MPI_Info);
 		global_spmd_info[0] = MPI_INFO_NULL;
 	}
 	if(global_spmd_request == NULL){
-		global_spmd_request = (MPI_Request *) R_Calloc(global_spmd_REQUEST_MAXSIZE, MPI_Request);
+		global_spmd_request = (MPI_Request *) Calloc(global_spmd_REQUEST_MAXSIZE, MPI_Request);
 		for(i = 0; i < global_spmd_REQUEST_MAXSIZE; i++){
 			global_spmd_request[i] = MPI_REQUEST_NULL;	
 		}
