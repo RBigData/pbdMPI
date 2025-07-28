e5fcd2f694750796414d8027e43065c247d136bd
George Ostrouchov <gostrouc@utk.edu>
2025-07-27 23:06:50 -0400

    simplify

--- inst/debug/testUnload.R
+++ inst/debug/testUnload.R
@@ -1,21 +1,3 @@
 # This is a test script for pbdMPI
-print("testUnload: About to load pbdMPI")
 library(pbdMPI)
-print("testUnload: Loaded pbdMPI")
-print("testUnload: Skipping sessionInfo() for now")
-# sessionInfo()
-print("testUnload: Size and Rank")
-print(comm.size())
-print(comm.rank())
-# print("testUnload: About to finalize pbdMPI")
-# finalize()
-# print("testUnload: Finalized pbdMPI")
-print("testUnload: About to detach pbdMPI")
 detach("package:pbdMPI", unload = TRUE)
-print("testUnload: Unloaded pbdMPI")
-Sys.sleep(2)
-print("testUnload: Sleep completed")
-
-print("Set filalize to FALSE")
-.pbd_env$SPMD.CT$mpi.finalize <- FALSE
-print("Last script line in testUnload.R")
